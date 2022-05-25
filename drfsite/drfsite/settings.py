0894e78a1caf3b35808d2b2092e84e000779e6ab
Evhen <sonykillstation@gmail.com>
2022-05-26 01:38:05 +0300

    ViewSet  упрощаем всЕ

--- drfsite/drfsite/settings.py
+++ drfsite/drfsite/settings.py
@@ -127,7 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-    #    'rest_framework.renderers.BrowsableAPIRender',
+        'rest_framework.renderers.BrowsableAPIRenderer',
 
     ]
 }
